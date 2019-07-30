60a832dbde8920376234188b23714e75669ca4f6
Rahul <rahul.bharadwaj.p@gmail.com>
2019-07-30 15:31:11 +0200

    few changes to account for erros

--- scripts/02_makeCSVTable_csmatch.R
+++ scripts/02_makeCSVTable_csmatch.R
@@ -52,38 +52,12 @@ for (echscore in wind.filelist){
   name <- sub("[.]csmatch", "", sub("[.]filter", "", sub("[.]snpmatch", "", filename)))
   likeLiwind <- try(read.table(echscore, header = F, as.is = TRUE))
   ScoreAcc <- try(read.table(paste(filename, ".scores.txt", sep =""), header = FALSE, as.is = TRUE))
-  jsonstat <- try(fromJSON(paste(filename, ".matches.json", sep = "")))
-  jsonstat_per <- try(fromJSON(paste(name, ".snpmatch.matches.json", sep = "")))
-  if(inherits(likeLiwind, 'try-error') | inherits(ScoreAcc, 'try-error') | inherits(jsonstat, 'try-error') | inherits(jsonstat_per, 'try-error')){
+  jsonstat_per <- try(fromJSON(paste(filename, ".scores.txt.matches.json", sep = "")))
+  if(inherits(likeLiwind, 'try-error') | inherits(ScoreAcc, 'try-error') |  inherits(jsonstat_per, 'try-error')){
     next
   }
-#  givparents <- sub("F2_p1_#", "", filename)
-#  expP1 <- unlist(strsplit(givparents, "x"))[1]
-#  expP2 <- unlist(strsplit(givparents, "x"))[2]
-
-  homowind <- which(likeLiwind$V7 == 1)
-  clean <- sort(table(likeLiwind$V1[homowind]))
-  clean <- as.data.frame(clean)
-  topHits <- rownames(tail(clean, 2))
-
-  PerHeterozygosity = c(PerHeterozygosity, jsonstat_per$percent_heterozygosity)
-  Overlap = c(Overlap, jsonstat$overlap[1])
-  FouParents1 <- c(FouParents1, as.character( jsonstat$parents$mother[1] ))
-  FouParents2 <- c(FouParents2, as.character( jsonstat$parents$father[1] ))
-  CountP1 <- c(CountP1, as.numeric( jsonstat$parents$mother[2] ))
-  CountP2 <- c(CountP2, as.numeric( jsonstat$parents$father[2] ))
-
-  ambWind <- which(likeLiwind$V7 < 20)
-  nclean <- sort(-table(likeLiwind$V1[ambWind]))
-  nclean <- as.data.frame(nclean)
-  nclean <- nclean[which(nclean$nclean <= -1),]
-
-  AmbiWindows <- c(AmbiWindows, paste(rownames(nclean), collapse = ":"))
-  AmbiWindowCount <- c(AmbiWindowCount, paste(as.numeric(-nclean), collapse = ":"))
-
-  NumWindows <- c(NumWindows, max(likeLiwind$V8))
+  
   plate <- c(plate, id)
-
   ranks <- order(-ScoreAcc$V4, na.last = TRUE, partial = ScoreAcc$V6)
   topscore <- ScoreAcc$V4[ranks[1]]
   topacc <- as.character(ScoreAcc$V1[ranks[1]])
@@ -94,6 +68,29 @@ for (echscore in wind.filelist){
   topnum <- length(which(ScoreAcc$V6 < 3.841))
   maxlike <- ScoreAcc$V5[ranks[1]]
   nextlike <- ScoreAcc$V6[ranks[2]]
+  PerHeterozygosity = c(PerHeterozygosity, jsonstat_per$percent_heterozygosity)
+  Overlap = c(Overlap, jsonstat_per$overlap[1])
+
+  if (file.exists( paste(filename, ".matches.json", sep = "") )){
+    jsonstat <- try(fromJSON(paste(filename, ".matches.json", sep = "")))
+    FouParents1 <- c(FouParents1, as.character( jsonstat$parents$mother[1] ))
+    FouParents2 <- c(FouParents2, as.character( jsonstat$parents$father[1] ))
+    CountP1 <- c(CountP1, as.numeric( jsonstat$parents$mother[2] ))
+    CountP2 <- c(CountP2, as.numeric( jsonstat$parents$father[2] ))
+  } else {
+    FouParents1 <- c(FouParents1, "")
+    FouParents2 <- c(FouParents2, "")
+    CountP1 <- c(CountP1, 0)
+    CountP2 <- c(CountP2, 0)
+  }
+
+  ambWind <- which(likeLiwind$V7 < 20)
+  nclean <- sort(-table(likeLiwind$V1[ambWind]))
+  nclean <- as.data.frame(nclean)
+  nclean <- nclean[which(nclean$nclean <= -1),]
+  AmbiWindows <- c(AmbiWindows, paste(rownames(nclean), collapse = ":"))
+  AmbiWindowCount <- c(AmbiWindowCount, paste(as.numeric(-nclean), collapse = ":"))
+  NumWindows <- c(NumWindows, max(likeLiwind$V8))
 
   LikeLihoodTopHit <- c(LikeLihoodTopHit, maxlike)
   LLRNextHit <- c(LLRNextHit, nextlike)
